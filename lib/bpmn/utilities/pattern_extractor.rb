044fd263b1c640a57ac2f271bde8e727fd25e82c
Łukasz <lukasz@lukasz-laptop.(none)>
2013-06-18 00:05:02 +0200

    Zmiana kolejności wyszukiwania. Nie powinno niczego naprawiać, a jednak naprawia o_O

--- lib/bpmn/utilities/pattern_extractor.rb
+++ lib/bpmn/utilities/pattern_extractor.rb
@@ -3,7 +3,7 @@ require 'active_support/inflector'
 module Bpmn
   module Utilities
     class PatternExtractor
-      ORDER = %i(sequence multiple_merge parallel_split exclusive_choice multi_choice simple_merge synchronization)
+      ORDER = %i( multiple_merge parallel_split exclusive_choice multi_choice simple_merge synchronization sequence )
 
       attr_reader :graph
 
